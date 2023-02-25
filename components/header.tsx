9032b535a604957868c33851ca13916d4400cc7a
ShineTiger <shinetiger.dev@gmail.com>
2023-02-25 17:32:31 +0900

    :sparkles: 수동 다크모드 전환 기능 구현

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,7 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 interface LayoutProps {
   title?: string;
@@ -6,23 +9,50 @@ interface LayoutProps {
 
 export default function Header({ title }: LayoutProps) {
   const { data: session, status } = useSession();
-  const loading = status === "loading";
+
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
+
+  //마운팅 이후 모드전환 가능케 하도록 초기화 시킴
+  useEffect(() => setMounted(true), []);
 
   return (
     <header className="container flex items-center justify-between h-16 px-4 mx-auto rounded dark:bg-gray-900">
-      <a rel="noopener noreferrer" href="#" aria-label="Homepage">
+      <Link href={"/"}>
         <h1>{title}</h1>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 32 32"
-          className="w-6 h-6 dark:text-violet-400"
-        >
-          <path d="M27.912 7.289l-10.324-5.961c-0.455-0.268-1.002-0.425-1.588-0.425s-1.133 0.158-1.604 0.433l0.015-0.008-10.324 5.961c-0.955 0.561-1.586 1.582-1.588 2.75v11.922c0.002 1.168 0.635 2.189 1.574 2.742l0.016 0.008 10.322 5.961c0.455 0.267 1.004 0.425 1.59 0.425 0.584 0 1.131-0.158 1.602-0.433l-0.014 0.008 10.322-5.961c0.955-0.561 1.586-1.582 1.588-2.75v-11.922c-0.002-1.168-0.633-2.189-1.573-2.742zM27.383 21.961c0 0.389-0.211 0.73-0.526 0.914l-0.004 0.002-10.324 5.961c-0.152 0.088-0.334 0.142-0.53 0.142s-0.377-0.053-0.535-0.145l0.005 0.002-10.324-5.961c-0.319-0.186-0.529-0.527-0.529-0.916v-11.922c0-0.389 0.211-0.73 0.526-0.914l0.004-0.002 10.324-5.961c0.152-0.090 0.334-0.143 0.53-0.143s0.377 0.053 0.535 0.144l-0.006-0.002 10.324 5.961c0.319 0.185 0.529 0.527 0.529 0.916z"></path>
-          <path d="M22.094 19.451h-0.758c-0.188 0-0.363 0.049-0.515 0.135l0.006-0.004-4.574 2.512-5.282-3.049v-6.082l5.282-3.051 4.576 2.504c0.146 0.082 0.323 0.131 0.508 0.131h0.758c0.293 0 0.529-0.239 0.529-0.531v-0.716c0-0.2-0.11-0.373-0.271-0.463l-0.004-0.002-5.078-2.777c-0.293-0.164-0.645-0.26-1.015-0.26-0.39 0-0.756 0.106-1.070 0.289l0.010-0.006-5.281 3.049c-0.636 0.375-1.056 1.055-1.059 1.834v6.082c0 0.779 0.422 1.461 1.049 1.828l0.009 0.006 5.281 3.049c0.305 0.178 0.67 0.284 1.061 0.284 0.373 0 0.723-0.098 1.027-0.265l-0.012 0.006 5.080-2.787c0.166-0.091 0.276-0.265 0.276-0.465v-0.716c0-0.293-0.238-0.529-0.529-0.529z"></path>
-        </svg>
-      </a>
+      </Link>
+
       <div className="items-center hidden space-x-8 lg:flex">
+        <label
+          htmlFor="DarkmodeToggle"
+          className="inline-flex items-center space-x-4 cursor-pointer text-gray-800"
+        >
+          <span className="relative">
+            <input
+              id="DarkmodeToggle"
+              type="checkbox"
+              checked={mounted}
+              className="hidden peer"
+              onChange={() =>
+                setTheme(resolvedTheme === "dark" ? "light" : "dark")
+              }
+            />
+            <div
+              className={`w-10 h-6 rounded-full shadow-inner bg-gray-600 ${
+                mounted &&
+                (resolvedTheme === "dark" ? "peer-checked:bg-violet-600" : "")
+              } `}
+            ></div>
+            <div
+              className={`absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow ${
+                mounted &&
+                (resolvedTheme === "dark"
+                  ? "peer-checked:right-0 peer-checked:left-auto"
+                  : "")
+              }  bg-gray-100`}
+            ></div>
+          </span>
+        </label>
         <div className="space-x-4">
           <a rel="noopener noreferrer" href="#">
             Link
@@ -41,7 +71,7 @@ export default function Header({ title }: LayoutProps) {
               e.preventDefault();
               signIn();
             }}
-            className="px-4 py-2 rounded-md dark:bg-violet-400 dark:text-gray-900"
+            className="px-4 py-2 rounded-md text-gray-50 bg-violet-600 dark:bg-violet-400 dark:text-gray-900"
           >
             Sign in
           </button>
@@ -52,7 +82,7 @@ export default function Header({ title }: LayoutProps) {
               e.preventDefault();
               signOut();
             }}
-            className="px-4 py-2 rounded-md dark:bg-violet-400 dark:text-gray-900"
+            className="px-4 py-2 rounded-md text-gray-50 bg-violet-600 dark:bg-violet-400 dark:text-gray-900"
           >
             Sign out
           </button>
