f241f18b74f15227f98c9adcd4acaca73eb005ba
ShineTiger <shinetiger.dev@gmail.com>
2023-03-07 16:03:32 +0900

    :construction: useMutation을 이용해서 서버와 통신

--- lib/client/useMutation.tsx
+++ lib/client/useMutation.tsx
@@ -9,6 +9,17 @@ export default function useMutation(
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<undefined | any>(undefined);
   const [error, setError] = useState<undefined | any>(undefined);
-  function mutation(data: any) {}
+  function mutation(data: any) {
+    setLoading(true);
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json().catch(() => {}))
+      .then((json) => setData(json))
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }
   return [mutation, { loading, data, error }];
 }
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,7 +5,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") res.status(401);
-  console.log(req.body.email);
-  res.status(200).end();
+  if (req.method !== "POST") res.status(401).end();
+  console.log(req.body);
+  res.json({ ok: true });
 }
