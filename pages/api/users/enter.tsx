e235556dbb8eee5acddd4024fe6715ab6de2deed
ShineTiger <shinetiger.dev@gmail.com>
2023-03-10 16:15:43 +0900

    :sparkles: upsert를 이용한 새로운 유저 생성

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,8 +3,14 @@ import withHandler from "@/lib/client/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next/types";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  res.json({ ok: true });
+  const formData = req.body;
+  const user = await client.user.upsert({
+    where: { ...formData },
+    create: { name: "annoymous", ...formData },
+    update: {},
+  });
+
+  return res.status(200).end();
 }
 
 export default withHandler("POST", handler);
