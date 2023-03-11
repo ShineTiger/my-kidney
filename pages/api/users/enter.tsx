0176cc9ed7e1a903ce8a8ba8665cee4bbabceb9a
ShineTiger <shinetiger.dev@gmail.com>
2023-03-11 16:26:20 +0900

    study : user 출력

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,12 +3,16 @@ import withHandler from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next/types";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const formData = req.body;
-  const user = await client.user.upsert({
-    where: { ...formData },
-    create: { name: "annoymous", ...formData },
-    update: {},
-  });
+  const { phone, email } = req.body;
+  let user;
+  if (email) {
+    user = await client.user.findUnique({ where: { email } });
+    if (!user) {
+      console.log("찾을 수 없으니 생성합니다.");
+      user = await client.user.create({ data: { name: "Annonymous", email } });
+    }
+    console.log(user);
+  }
 
   return res.status(200).end();
 }
