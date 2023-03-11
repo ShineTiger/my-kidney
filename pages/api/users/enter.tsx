c01b698047e73befb3a97ff63c49f1ccb7b4832c
ShineTiger <shinetiger.dev@gmail.com>
2023-03-11 21:29:02 +0900

    :monocle_face: User를 relation하는 model을 생성
    DB에 어떻게 데이터가 추가되고 참조하는지 알아봄

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,18 +1,35 @@
 import client from "@/libs/server/client";
 import withHandler from "@/libs/server/withHandler";
+import { connect } from "http2";
 import { NextApiRequest, NextApiResponse } from "next/types";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
-  let user;
-  if (email) {
-    user = await client.user.findUnique({ where: { email } });
-    if (!user) {
-      console.log("찾을 수 없으니 생성합니다.");
-      user = await client.user.create({ data: { name: "Annonymous", email } });
-    }
-    console.log(user);
-  }
+  const address = phone ? { phone: +phone } : { email };
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+
+  const user = await client.user.upsert({
+    where: {
+      ...address,
+    },
+    create: {
+      name: "Anonymous",
+      ...address,
+    },
+    update: {},
+  });
+  const token = await client.token.create({
+    data: { payload: payload, user: { connect: { id: user.id } } },
+  });
+  console.log(token);
+  // if (email) {
+  //   user = await client.user.findUnique({ where: { email } });
+  //   if (!user) {
+  //     console.log("찾을 수 없으니 생성합니다.");
+  //     user = await client.user.create({ data: { name: "Annonymous", email } });
+  //   }
+  //   console.log(user);
+  // }
 
   return res.status(200).end();
 }
