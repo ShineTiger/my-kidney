aadf6794698a419af580c3f45bfa7a5b5002d7fd
ShineTiger <shinetiger.dev@gmail.com>
2023-03-21 20:16:22 +0900

    :sparkles: 프로필 페이지 구현

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,9 +1,52 @@
 import Layout from "@/components/layout";
+import { GetServerSidePropsContext } from "next";
+import { getServerSession } from "next-auth";
 import { useSession } from "next-auth/react";
 import React from "react";
+import { authOptions } from "./api/auth/[...nextauth]";
 
 export default function mypage() {
-  const { data } = useSession();
+  const { data: session } = useSession();
+  const image = JSON.stringify(session?.user?.image);
+  if (session) {
+    return (
+      <Layout>
+        <div className="p-6 sm:p-12 dark:bg-gray-900 dark:text-gray-100">
+          <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
+            <img
+              src={image.replace(/\"/gi, "")}
+              alt=""
+              className="self-center flex-shrink-0 w-24 h-24 border rounded-full md:justify-self-start"
+            />
+            <div className="flex flex-col">
+              <h4 className="text-lg font-semibold text-center md:text-left">
+                Name
+              </h4>
+              <p className="dark:text-gray-400">
+                <span></span>
+                {JSON.stringify(session.user?.name)}
+              </p>
+              <h4 className="text-lg font-semibold text-center md:text-left">
+                Email
+              </h4>
+              <p className="dark:text-gray-400">
+                <span></span>
+                {JSON.stringify(session.user?.email)}
+              </p>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    );
+  } else {
+    return <Layout>"로그인하세요"</Layout>;
+  }
+}
 
-  return <Layout>{JSON.stringify(data, null, 2)}</Layout>;
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  return {
+    props: {
+      session: await getServerSession(context.req, context.res, authOptions),
+    },
+  };
 }
