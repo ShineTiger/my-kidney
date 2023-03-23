60318c87a6f69494c5240690124514c7e1197acc
ShineTiger <shinetiger.dev@gmail.com>
2023-03-23 18:00:46 +0900

    :recycle: PrismaClient 인스턴스화
    PrismaClient를 한번만 생성하고 재사용하도록 한다

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -3,8 +3,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "@/libs/server/prisma";
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
