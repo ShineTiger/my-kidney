9ceccb3b32e3d4aec9c76abbd5270b729d3e6fca
ShineTiger <shinetiger.dev@gmail.com>
2023-03-26 20:50:41 +0900

    :sparkles: 포스트 삭제기능 구현
    세션을 확인하고 작성자와 일치하면 포스트 삭제버튼이 렌더링되고
    삭제요청을 보냅니다

--- pages/api/posts/[id].ts
+++ pages/api/posts/[id].ts
@@ -1,6 +1,8 @@
 import prisma from "@/libs/server/prisma";
 import withHandler from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
@@ -8,21 +10,29 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!id) {
     return res.status(400).json({ error: "id를 찾을 수 없습니다" });
   } else {
-    if (req.method === "DELETE") {
-      await prisma.post.delete({ where: { id: id.toString() } });
-      res.status(200).json({ message: "포스트가 삭제되었습니다" });
-    }
-    const post = await prisma.post.findUnique({
-      where: { id: id.toString() },
-      include: {
-        user: {
-          select: { id: true, name: true, image: true },
+    if (req.method === "GET") {
+      const post = await prisma.post.findUnique({
+        where: { id: id.toString() },
+        include: {
+          user: {
+            select: { id: true, name: true, image: true },
+          },
         },
-      },
-    });
-    console.log(post);
-    res.json(post);
+      });
+      res.json(post);
+    }
+
+    const session = await getServerSession(req, res, authOptions);
+    if (session) {
+      if (req.method === "DELETE") {
+        await prisma.post.delete({ where: { id: id.toString() } });
+        res.status(200).json({ message: "포스트가 삭제되었습니다" });
+      }
+      // if(req.method === "POST"){
+      //   const alreadyExists = Boolean(await prisma.user.findUnique({where:email}))
+      // }
+    }
   }
 }
 
-export default withHandler(["GET", "DELETE"], handler);
+export default withHandler(["GET", "DELETE", "POST"], handler);
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,20 +1,22 @@
 import Layout from "@/components/layout";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 function PostDetail() {
   const router = useRouter();
-  const session = useSession();
+  const { data: session } = useSession();
 
   //todo : 삭제되었으면 null대신 리다이렉트
   const { data } = useSWR(
     router.query.id ? `/api/posts/${router.query.id}` : null,
     fetcher
   );
+
+  const [isPostAuthor, setIsPostAuthor] = useState(false);
 
   async function deletePost() {
     alert("정말로 해당 포스트를 삭제하시겠습니까?");
@@ -29,10 +31,23 @@ function PostDetail() {
   if (!data) {
     return <div>Loading..</div>;
   }
+
+  useEffect(() => {
+    if (data && session) {
+      setIsPostAuthor(data.user.name === session?.user?.name);
+    } else {
+      setIsPostAuthor(false);
+    }
+  }, [data, session]);
+
   return (
     <Layout>
       <p>{data.title}</p>
-      <button onClick={deletePost}>[삭제]</button>
+      {isPostAuthor && (
+        <div>
+          <button onClick={deletePost}>[삭제]</button>
+        </div>
+      )}
     </Layout>
   );
 }
