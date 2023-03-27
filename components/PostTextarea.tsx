edc245588d21c6b7df5d820a5bf7ca46bb420f8d
ShineTiger <shinetiger.dev@gmail.com>
2023-03-27 22:43:50 +0900

    :recycle: input 컴포넌트 분리
    재사용성을 위해 input 컴포넌트를 분리했다

--- components/PostTextarea.tsx
+++ components/PostTextarea.tsx
@@ -7,7 +7,7 @@ interface InputProps {
   type?: string;
 }
 
-const PostInput = ({ name, label, type }: InputProps) => {
+const PostTextarea = ({ name, label, type }: InputProps) => {
   const {
     register,
     formState: { errors },
@@ -18,14 +18,13 @@ const PostInput = ({ name, label, type }: InputProps) => {
       <label className="block uppercase tracking-wide text-gray-700 dark:text-gray-200 text-m font-bold mb-2">
         {label}
       </label>
-      <input
+      <textarea
         {...register(name, {
-          required: "제목을 입력해주세요",
+          required: "내용을 입력해주세요",
         })}
-        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
         id={name}
-        type={type}
-      />
+      ></textarea>
       <span className="label-text-alt text-red-600">
         {errors.root && <p className="text-red-600">{errors?.root?.message}</p>}
       </span>
@@ -33,4 +32,4 @@ const PostInput = ({ name, label, type }: InputProps) => {
   );
 };
 
-export default PostInput;
+export default PostTextarea;
--- pages/posts/upload.tsx
+++ pages/posts/upload.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/layout";
-import PostForm from "@/components/PostForm";
+import PostInput from "@/components/PostInput";
+import PostTextarea from "@/components/PostTextarea";
 import useMutation from "@/libs/client/useMutation";
 import { Post } from "@prisma/client";
 import { NextPage } from "next";
@@ -7,7 +8,7 @@ import router from "next/router";
 import React, { useEffect, useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 
-interface PostType {
+interface postType {
   title: string;
   content: string;
 }
@@ -19,7 +20,17 @@ interface UploadPostMutation {
 const upload: NextPage = () => {
   const [submit, { loading, data }] =
     useMutation<UploadPostMutation>("/api/posts/submit");
-  const methods = useForm();
+  const {
+    handleSubmit,
+    formState: { errors },
+  } = useForm<postType>({ mode: "onChange" });
+
+  const methods = useForm({ mode: "onChange" });
+
+  const onValid = (validForm: any) => {
+    if (loading) return;
+    submit(validForm);
+  };
 
   useEffect(() => {
     if (data) {
@@ -28,14 +39,64 @@ const upload: NextPage = () => {
     }
   }, [data, router]);
 
-  const onValid = (validForm: PostType) => {
-    if (loading) return;
-  };
-
   return (
     <Layout>
       <FormProvider {...methods}>
-        <PostForm loading={loading} data={data} methods={methods} />
+        <form
+          onSubmit={methods.handleSubmit(onValid)}
+          className="w-full max-w-lg my-0 mx-auto"
+        >
+          <div className="flex flex-wrap -mx-3 mb-6">
+            {/* <div className="w-full px-3">
+              <label className="block uppercase tracking-wide text-gray-700 dark:text-gray-200 text-m font-bold mb-2">
+                제목
+              </label>
+              <input
+              {...register("title", {
+                required: "제목을 입력해주세요",
+              })}
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="title"
+              type="text"
+            />
+              
+              <span className="label-text-alt text-red-600">
+                {errors.title ? errors.title.message : ""}
+              </span>
+            </div> */}
+            <PostInput name="title" label="제목" type="text" />
+          </div>
+
+          <div className="flex flex-wrap -mx-3 mb-6">
+            {/* <div className="w-full px-3">
+            <label className="block uppercase tracking-wide text-gray-700 dark:text-gray-200 text-m font-bold mb-2">
+              내용
+            </label>
+            <textarea
+              {...register("content", {
+                required: "내용을 입력해주세요",
+              })}
+              className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+              id="content"
+            ></textarea>
+            <span className="label-text-alt text-red-600">
+              {errors.content ? errors.content.message : ""}
+            </span>
+          </div> */}
+            <PostTextarea name="content" label="내용" />
+          </div>
+          <div className="md:flex md:items-center">
+            <div className="md:w-1/3">
+              <button
+                className="shadow bg-violet-600 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                type="submit"
+              >
+                {loading ? "발행 중" : "발행하기"}
+              </button>
+            </div>
+            <div className="md:w-2/3"></div>
+          </div>
+        </form>
       </FormProvider>
     </Layout>
   );
