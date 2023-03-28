050bf3004bb8faba5409ed5380cdfefbe43cc47b
ShineTiger <shinetiger.dev@gmail.com>
2023-03-28 15:18:11 +0900

    :recycle: 조건렌더링을 삼항연산자로 변경함
    세션이 있는지 확인하고 포스트를 작성한 유저의 name과 현재 세션이 가진 name이 일치하는지 최종적으로 확인하고 삭제 버튼을 렌더링 합니다

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,8 +1,27 @@
 import Layout from "@/components/layout";
+import { Post } from "@prisma/client";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
+interface GetPostType {
+  post:
+    | (Post & {
+        user: {
+          id: string;
+          name: string | null;
+          image: string | null;
+        };
+      })
+    | null;
+  user:
+    | {
+        id: string;
+        name: string | null;
+        image: string | null;
+      }
+    | undefined;
+}
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
@@ -11,12 +30,10 @@ function PostDetail() {
   const { data: session } = useSession();
 
   //todo : 삭제되었으면 null대신 리다이렉트
-  const { data } = useSWR(
+  const { data } = useSWR<GetPostType>(
     router.query.id ? `/api/posts/${router.query.id}` : null,
     fetcher
   );
-
-  const [isPostAuthor, setIsPostAuthor] = useState(false);
 
   async function deletePost() {
     alert("정말로 해당 포스트를 삭제하시겠습니까?");
@@ -31,19 +48,11 @@ function PostDetail() {
   if (!data) {
     return <div>Loading..</div>;
   }
-
-  useEffect(() => {
-    if (data && session) {
-      setIsPostAuthor(data.user.name === session?.user?.name);
-    } else {
-      setIsPostAuthor(false);
-    }
-  }, [data, session]);
-
   return (
     <Layout>
-      <p>{data.title}</p>
-      {isPostAuthor && (
+      <p>{data.post?.title}</p>
+      <p>{data.post?.content}</p>
+      {session && data.post?.user.name === session?.user?.name && (
         <div>
           <button onClick={deletePost}>[삭제]</button>
         </div>
